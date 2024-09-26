9399759f22a5247a152138df71c316c21a0d07d3
Arunarchitect <intformbase@gmail.com>
2024-09-26 13:14:51 +0530

    added usersignin signupfrontend logics

--- client/src/components/Inputbox.jsx
+++ client/src/components/Inputbox.jsx
@@ -1,16 +1,32 @@
-const InputBox = ({ name, type, id, value, placeholder }) => {
-    return (
-      <div className="relative w-full mb-4">
-        <input
-          type={type}
-          name={name}
-          placeholder={placeholder}
-          defaultValue={value}
-          id={id}
-        />
-      </div>
-    );
-  };
-  
-  export default InputBox;
-  
+import React, { useState } from "react";
+import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
+
+const InputBox = ({ name, type, id, value, placeholder, icon }) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  return (
+    <div className="relative w-full mb-4">
+      <input
+        type={type === 'password' ? (passwordVisible ? "text" : "password") : type}
+        name={name}
+        placeholder={placeholder}
+        defaultValue={value}
+        id={id}
+        className="input-box"
+      />
+      <i className="input-icon">
+        {icon} {/* Display the passed icon */}
+      </i>
+      {type === "password" && (
+        <div
+          className="absolute right-4 top-3 cursor-pointer"
+          onClick={() => setPasswordVisible((currentVal) => !currentVal)}
+        >
+          {!passwordVisible ? <IoEyeOffOutline  /> : <IoEyeOutline />}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InputBox;
--- client/src/containers/User/UserAuthForm.jsx
+++ client/src/containers/User/UserAuthForm.jsx
@@ -1,20 +1,70 @@
 import React from "react";
 import InputBox from "../../components/Inputbox";
+import { FaUser, FaEnvelope } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import AnimationWrapper from "../../common/page-anime";
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex item-center justify-center">
-      <form className="w-[80%] max-4-[400px]">
-        <h1 className="text-4xl font-gelasio capitalise">
-          {type == "sign-in" ? "Welcomeback" : "Join us Today"}
-        </h1>
-        {type != "sign-in" ? (
-          <InputBox name="fullname" type="text" placeholder="fullname" />
-        ) : (
-          ""
-        )}
-      </form>
-    </section>
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex item-center justify-center">
+        <form className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize">
+            {type === "sign-in" ? "Welcome back" : "Join us Today"}
+          </h1>
+          {type !== "sign-in" && (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full name"
+              icon={<FaUser />}
+            />
+          )}
+          <InputBox
+            name="email"
+            type="email"
+            placeholder="Email"
+            icon={<FaEnvelope />}
+          />
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon={<RiLockPasswordFill />}
+          />
+          <button className="btn-dark center mt-2" type="submit">
+            {type.replace("-", " ")}
+          </button>
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-50 uppercase text-black font-bold">
+            <hr className="w-1/2  border-black" />
+            <p>or</p>
+            <hr className="w-1/2  border-black" />
+          </div>
+          <button className="btn-dark flex items-center justify-center gap-2 w-full py-2 text-sm sm:text-base">
+            <FcGoogle className="text-lg" />
+            <span className="hidden sm:block">Continue with Google</span>
+            <span className="block sm:hidden">Continnue with Google</span>
+          </button>
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark text-sm text-center">
+              No Account ?
+              <Link to="/signup" className="underline text-black  text-sm ml-1">
+                Join Us today!
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark text-sm text-center">
+              Already a Member ?
+              <Link to="/signin" className="underline text-black  text-sm ml-1">
+                Sign in Here{" "}
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
   );
 };
 
